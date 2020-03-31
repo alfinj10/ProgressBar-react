80af7075c90a1a396f913d6b07aa8f086e8502b2
Alfin Jose <alfin.jose@publicissapient.com>
2020-03-31 19:11:09 +0100

    build folder

---
+++ build/precache-manifest.13bbf50c7b63ccacb764a6e6540d81f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6868d3399caea5ad2e258509c77873a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2beb2778c3d46566dfe",
+    "url": "/static/css/main.16ed6b97.chunk.css"
+  },
+  {
+    "revision": "a02053bc983005831080",
+    "url": "/static/js/2.5679fb7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5679fb7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2beb2778c3d46566dfe",
+    "url": "/static/js/main.d16c3183.chunk.js"
+  },
+  {
+    "revision": "139606d874b00c2015bb",
+    "url": "/static/js/runtime-main.2d4d7c86.js"
+  }
+]);
